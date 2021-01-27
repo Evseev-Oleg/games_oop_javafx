7b4767db8bccfc04817db1c1c112d691ffd72938
Evseev Oleg <oleg063053@gmail.com>
2021-01-27 11:21:09 +0400

    2. Сокобан. Тестовое задание. [#456351]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -7,6 +7,7 @@ public class Win {
             if (board[i][i] == 1) {
                 if (checkVertical(board, i) || checkHorizontal(board, i)) {
                     rsl = true;
+                    break;
                 }
             }
         }
